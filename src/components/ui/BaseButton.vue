ca292052cf8adca84c56358d8a978e126e78892e
lluuk <luk.zapolski@gmail.com>
2024-05-09 13:36:47 +0200

    fix: add correct styles for activated mode

--- src/components/ui/BaseButton.vue
+++ src/components/ui/BaseButton.vue
@@ -10,8 +10,12 @@ withDefaults(defineProps<Props>(), {
 
 <template>
   <button
-    class="bg-white hover:bg-gray-100 text-gray-600 font-semibold p-2 border border-gray-400 rounded shadow"
-    :class="isActivated && 'bg-blue-500 text-white border-blue-700'"
+    class="font-semibold p-2 border border-gray-400 rounded shadow"
+    :class="
+      isActivated
+        ? 'bg-blue-500 text-white border-blue-700'
+        : 'bg-white hover:bg-gray-100 text-gray-600'
+    "
   >
     <slot />
   </button>
